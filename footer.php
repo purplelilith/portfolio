82c6ba2a949c1b10035f8f62c8dc4d95f5ca6562
jandi kim <acidlime1987@naver.com>
2022-12-25 02:51:46 +0900

    집에서 작업함(푸터, 퍼블리싱 배경색)

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
 <section class="footer">
   <img src="./img/footer.png" alt="">
-  <h2>Thank You</h2>  
+  <div class="text_box">
+    <p class="top">
+      <em>Jandi Kim</em>
+      <em>first portfolio 2022 ver.</em>
+    </p>
+    <h2>Thank You</h2>
+    <p class="bottom">contact me if you like my portfolio</p> 
+  </div> 
 </section>
